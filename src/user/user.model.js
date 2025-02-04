445b525125c07db66966ed1a29bff7c47733d2b5
Josue Garcia <jgarcia-2023324@kinal.edu.gt>
2025-02-04 16:16:42 -0600

    add: se agrego estructura.

---
+++ src/user/user.model.js
@@ -0,0 +1,82 @@
+import { Schema, model } from "mongoose";
+
+//Esquema del documento que voy a almacenar en mongo
+const userSchema = Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        maxLength: [25, "Name cannot exceed 25 characters"]
+
+    },
+    surname: {
+        type: String,
+        required: [true, "surname is required"],
+        maxLength: [25, "surname cannot exceed 25 characters"]
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true
+
+    },
+
+    password:{
+        type: String,
+        minLength: 8,
+        required: true
+
+
+    },
+
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        //Para que sea unico
+        unique: true
+    },
+
+    profilePicture: {
+        type: String,
+
+    },
+
+    phone: {
+        type: String,
+        required: true,
+        minLength: 8,
+        maxLength: 8
+
+    },
+
+    course: {
+        type: String,
+        required: true
+        
+    },
+
+    role: {
+        type: String,
+        required: true,
+        enum: ["STUDENT_ROLE", "TEACHER_ROLE"]
+    },
+
+    status: {
+        type: Boolean,
+        default: true
+    }
+},
+    {
+        versionKey: false,
+        timestamps: true
+    }
+)
+
+userSchema.methods.toJSON = function(){
+    const{ password, _id, ...user} = this.toObject()
+    user.uid = _id
+    return user
+
+}
+
+// Exportamos el esquema para utilizarlo
+export default model("User", userSchema)
